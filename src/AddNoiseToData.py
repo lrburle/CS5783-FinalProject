f14135f68accf5770cf532dc87ef8b069769efc2
aking123457 <alex.sensintaffar@okstate.edu>
2022-11-08 16:02:50 -0600

    basic functionality to add noise to .wav files

---
+++ src/AddNoiseToData.py
@@ -0,0 +1,80 @@
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import models
+# from CreateData import *
+
+def get_spectrogram(waveform):
+    # Convert the waveform to a spectrogram via a STFT.
+    spectrogram = tf.signal.stft(
+        waveform, frame_length=255, frame_step=128)
+    # Obtain the magnitude of the STFT.
+    spectrogram = tf.abs(spectrogram)
+    # Add a `channels` dimension, so that the spectrogram can be used
+    # as image-like input data with convolution layers (which expect
+    # shape (`batch_size`, `height`, `width`, `channels`).
+    spectrogram = spectrogram[..., tf.newaxis]
+    return spectrogram
+
+def plot_spectrogram(spectrogram, ax):
+    if len(spectrogram.shape) > 2:
+        assert len(spectrogram.shape) == 3
+        spectrogram = np.squeeze(spectrogram, axis=-1)
+    # Convert the frequencies to log scale and transpose, so that the time is
+    # represented on the x-axis (columns).
+    # Add an epsilon to avoid taking a log of zero.
+    log_spec = np.log(spectrogram.T + np.finfo(float).eps)
+    height = log_spec.shape[0]
+    width = log_spec.shape[1]
+    X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
+    Y = range(height)
+    ax.pcolormesh(X, Y, log_spec)
+
+def squeeze(audio, labels):
+    audio = tf.squeeze(audio, axis=-1)
+    return audio, labels
+
+# DATASET_PATH = "data/mini_speech_commands/down/0a9f9af7_nohash_0.wav"
+DATASET_PATH = "data/mini_speech_commands"
+
+data_dir = pathlib.Path(DATASET_PATH)
+
+train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+    directory=data_dir,
+    batch_size=64,
+    validation_split=0.2,
+    seed=0,
+    output_sequence_length=16000,
+    subset='both')
+
+label_names = np.array(train_ds.class_names)
+
+train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
+sets = 6
+for example_audio, example_labels in train_ds.take(sets):  
+    print(example_audio.shape)
+    print(example_labels.shape)
+
+figure, axes = plt.subplots(2, sets,figsize=(12, 8))
+# sound = 0
+for i in range(sets):
+    label = label_names[example_labels[i]]
+    waveform = example_audio[i]
+    spectrogram = get_spectrogram(waveform)
+
+    timescale = np.arange(waveform.shape[0])
+    axes[0][i].plot(timescale, waveform.numpy())
+    axes[0][i].set_title('Waveform ' + label)
+    axes[0][i].set_xlim([0, 16000])
+
+    plot_spectrogram(spectrogram.numpy(), axes[1][i])
+    axes[1][i].set_title('Spectrogram')
+    plt.suptitle(label.title())
+plt.show()
---
+++ src/NewAddNoiseToData.py
@@ -0,0 +1,54 @@
+import librosa
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def createFileList(path, outFile):
+    dir_list = os.listdir(path)
+
+    w = open(outFile, "w")
+
+    for i in range(len(dir_list)):
+        w.write(dir_list[i] + "\n")
+
+def prepareFiles():
+    createFileList("A:/OSU/Semester 9/CS 5783/CS5783-FinalProject/src/data/mini_speech_commands/down/", "data/FileListDown.txt")
+    createFileList("A:/OSU/Semester 9/CS 5783/CS5783-FinalProject/src/data/mini_speech_commands/go/", "data/FileListGo.txt")
+    createFileList("A:/OSU/Semester 9/CS 5783/CS5783-FinalProject/src/data/mini_speech_commands/left/", "data/FileListLeft.txt")
+    createFileList("A:/OSU/Semester 9/CS 5783/CS5783-FinalProject/src/data/mini_speech_commands/no/", "data/FileListNo.txt")
+    createFileList("A:/OSU/Semester 9/CS 5783/CS5783-FinalProject/src/data/mini_speech_commands/right/", "data/FileListRight.txt")
+
+
+
+def main():
+    word = "down"
+    locationWord = word[0].upper() + word[1:len(word)]
+
+    f = open("data/FileList" + locationWord + ".txt", 'r')
+    lines = [line.rstrip() for line in f][1]
+
+    location = "A:/OSU/Semester 9/CS 5783/CS5783-FinalProject/src/data/mini_speech_commands/" + word + "/" + lines
+    # print(lines)
+    signal, sr = librosa.load(location)
+    
+    # plt.plot(signal)
+    fig, axes = plt.subplots(3, 1)
+    axes[0].plot(signal)
+    axes[0].set_title(lines)
+
+    RMS = math.sqrt(np.mean(signal**2))
+    # RMS_n = math.sqrt(RMS**2/(10**(SNR/10)))
+    noise=np.random.normal(0, RMS, signal.shape[0])
+    axes[1].plot(noise)
+    axes[1].set_title("noise")
+
+    signal_noise = signal+noise
+    axes[2].plot(signal_noise)
+    axes[2].set_title("signal noise")
+
+    plt.show()
+
+main()
+
+# prepareFiles()
