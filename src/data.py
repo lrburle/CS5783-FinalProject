72b65ed4a16e0f1b2291cba2dfa7e660d66f8d5e
aking123457 <alex.sensintaffar@okstate.edu>
2022-11-17 15:57:26 -0600

    Update data.py

--- src/data.py
+++ src/data.py
@@ -318,7 +318,7 @@ class Data:
 			# print(type(waveform), waveform)
 
 			yFile = file[0:file.index("noise")-1] + ".wav"
-			dataRate, waveform = wavfile.read(PATH_Y + yFile)
+			waveform, dataRate = librosa.load(PATH_Y + yFile)
 			y_data.append(waveform)
 			data_rate_Y.append(dataRate)
 
